d876ee83347d1a3b14320e2ab7ceb528478cae00
Gustavo Muñoz <timbergus@gmail.com>
2017-01-07 02:25:08 +0100

    Add library for common headers.

---
+++ std_lib_facilities.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+// int, double, char, string, bool
+
+inline string keep_window_open() {
+  string ch;
+  cin >> ch;
+  return ch;
+}
